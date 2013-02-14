0253478bb727a0f1e2fe8ee8d723a5756c1d4f13
Phil Soga <phil@discoverygarden.ca>
2013-02-14 16:56:34 -0400

    reverting back to original regex

--- js/track_ds_downloads.js
+++ js/track_ds_downloads.js
@@ -1,4 +1,4 @@
-(function ($) {
+/*(function ($) {
 
   Drupal.behaviors.islandoraGATrack = {
     attach: function (context, settings) {
@@ -55,6 +55,29 @@
         }
                    _gaq.push(['_trackPageview', trackPageViewString]);        _gaq.push(["_setAccount", ""]); 
       }    } 
+  };
+
+})(jQuery);*/
+
+
+(function ($) {
+
+  Drupal.behaviors.islandoraGATrack = {
+    attach: function (context, settings) {
+      $('td.datastream-download a').click(function() {
+        var url = $(this).attr('href');
+        _gaq.push(['_trackPageview', url]);
+      });
+      //this is for the google analytics to track site searches
+
+      var path = window.location.pathname;
+      if (path.indexOf("islandora/solr/search") != -1) {
+      var r = new RegExp("/islandora/solr/search/([^/]*)/?(.*)?");
+      var m = path.match(r);
+      _gaq.push(['_trackPageview', '/islandora/search/?q='+m[1]]);
+      _gaq.push(["_setAccount", ""]);
+      }
+    }
   };
 
 })(jQuery);
